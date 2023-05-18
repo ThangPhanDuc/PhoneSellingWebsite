0ddc3347456c8bf41a884723aa96a57b57fabd0a
Phan Duc Thang PTIT <93048487+ThangPhanDuc@users.noreply.github.com>
2023-05-18 23:26:42 +0700

    Delete product-management directory

--- product-management/src/main/java/vn/com/devmaster/services/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package vn.com.devmaster.services.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import vn.com.devmaster.services.domain.User;
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-}
